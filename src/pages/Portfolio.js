aef9660cb224382e64f9bfe2604b57a67019bac8
nicolas <nicolas-dutertre@bbox.fr>
2022-04-20 10:47:26 +0200

    Ajout des styles SASS

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,6 +4,13 @@ const Portfolio = () => {
     return (
         <div className="portfolio">
             <Navigation />
+            <p> 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod quam eros, ut pulvinar ante cursus et. Proin non lacus leo. In id erat ipsum. Sed a est nisi. In pulvinar, lorem ac hendrerit pretium, magna risus volutpat risus, ut feugiat risus mi eu risus. Sed nulla ligula, sagittis cursus tristique in, convallis sed augue. Cras tincidunt interdum nisi, at maximus justo accumsan quis. Maecenas vulputate ullamcorper sapien, tincidunt ultrices dolor congue non. Nam at efficitur eros, ut hendrerit ligula. Proin at risus feugiat, mollis ante id, malesuada nibh.
+
+                Maecenas dapibus dolor nec dui finibus semper. Mauris quis lacus non urna cursus convallis ac non nunc. Integer suscipit id dui non sodales. Nunc purus nunc, semper in sollicitudin in, efficitur in eros. Nullam fringilla magna in vestibulum vestibulum. Nullam neque est, ultrices nec mollis eu, cursus a sem. Sed consectetur, libero at posuere elementum, quam nisl cursus nulla, malesuada molestie purus lectus eu neque. Vestibulum varius ac odio vel porta. In hac habitasse platea dictumst.
+
+                Cras iaculis dui vitae libero laoreet ultricies. Duis tincidunt, justo vel dignissim posuere, libero magna facilisis lectus, quis suscipit diam libero vitae urna. Duis vel lectus dapibus, consequat est vel, congue orci. Duis posuere facilisis volutpat. Sed consectetur, magna id gravida sollicitudin, nunc nibh malesuada ex, at pulvinar lacus neque in nisi. Sed euismod, quam a sodales commodo, tortor magna placerat purus, ut sodales nisi neque ac nunc. Suspendisse potenti. Nullam tincidunt diam eu libero facilisis consequat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas eget justo eu mi gravida accumsan. Maecenas egestas, tellus vel viverra mollis, lorem odio malesuada sapien, id egestas purus augue id lorem. Donec nibh urna, posuere eu semper non, mattis non dui. Nulla at pellentesque erat. Donec tellus massa, tincidunt sit amet pretium non, mattis non nunc. Vestibulum ac dapibus justo. Suspendisse pretium ligula ut erat tempor molestie. 
+            </p>
         </div>
     );
 };
